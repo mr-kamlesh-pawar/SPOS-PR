e17f085d1844563341a84a60b3f0810aeee85e51
Kamlesh Pawar <kamleshpawar5278@gmail.com>
2024-11-10 10:47:24 +0530

    new updated

--- Pass1Micro/Pass1Micro.java
+++ Pass1Micro/Pass1Micro.java
@@ -1,5 +1,4 @@
-
-
+package Pass1Micro;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
--- Pass2/Pass2.java
+++ Pass2/Pass2.java
@@ -1,3 +1,4 @@
+package Pass2;
 import java.io.*;
 import java.util.*;
 
